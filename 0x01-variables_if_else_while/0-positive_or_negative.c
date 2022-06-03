8767c102093e778a3ca2dee0c0473915a5797502
Tegisty Hailay <tigisthay13@gmail.com>
2022-06-03 08:02:13 -0700

    ./0-positive_or_negativ

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/*
- *main - Entry Point
+
+/**
+ * main - printing random number as positive , negative or zero
  *
- *Return : always 0 (success)
+ * Return: Always 0 (success)
  */
+
 int main(void)
 {
 	int n;
