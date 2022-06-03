7141bc969130742f6c5d39aeb4aed845965de09c
Tegisty Hailay <tigisthay13@gmail.com>
2022-06-03 09:57:12 -0700

    ./100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-/*
- * tg
- * cs
+/**
+ * main - prints all possible combination of two digits
+ * Return: Always 0.
  */
 int main(void)
 {
@@ -17,13 +17,12 @@ int main(void)
 				putchar((a % 10) + '0');
 				putchar((b % 10) + '0');
 				if (a == 8 && b == 9)
-					continue;				
+					continue;
 				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
 	putchar('\n');
-
 	return (0);
 }
