9300c25b43937e427d02be89b1fd1f2711e86e82
Tegisty Hailay <tigisthay13@gmail.com>
2022-06-03 01:50:53 -0700

    ./8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+ * main
+ * return
+ */
+int main(void)
+{
+	int n;
+	char c;
+
+	for (n  = 0; n  <10; n++)
+		putchar((n % 10) + '0');
+
+	for  (c = 'a'; c <= 'f'; c++)
+		putchar(c);
+	putchar('\n');
+
+	return (0);
+}
+	
