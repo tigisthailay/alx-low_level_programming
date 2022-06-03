7937ed1782d34d3ca475de1fee8b936a74cb9661
Tegisty Hailay <tigisthay13@gmail.com>
2022-06-03 00:54:39 -0700

    ./2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main: returns in lowercase
+ * return always 0.
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+	
+	return (0);
+}
