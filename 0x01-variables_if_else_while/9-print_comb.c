3166aebd5af23a4a06352c19f8227045a6109d35
Tegisty Hailay <tigisthay13@gmail.com>
2022-06-03 01:59:00 -0700

    ./9-print_comb | cat -e

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/*
+ * tg
+ * cs
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar((n % 10) + '0');
+		if (n == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	
+	return (0);
+}
+
