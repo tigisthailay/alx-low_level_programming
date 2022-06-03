ef9eb2e9d11ea12227c3d301cf61f21b422e7290
Tegisty Hailay <tigisthay13@gmail.com>
2022-06-03 02:55:40 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/*
+ * tg
+ * cs
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = '0'; a < '9'; a++)
+	{
+		for (b = a + 1; b <= '9'; b++)
+		{
+			for (c = b + 1; c <= '9'; c++)
+			{
+				if ((a != b) != c)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+
+					if (a == '7' && b == '8')
+						continue;
+
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
+
