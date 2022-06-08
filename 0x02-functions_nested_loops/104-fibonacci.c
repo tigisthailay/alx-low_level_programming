b62129af866a089243c8538eaa25a2276fcd614e
Tegisty Hailay <tigisthay13@gmail.com>
2022-06-08 01:53:33 -0700

    104-fibonacci.c2

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,18 +12,18 @@ int main(void)
 
 	fib[0] = 1;
 	fib[1] = 2;
-	printf("%ld, %ld, ", fib[0], fib[1]);
+	printf(", , ", fib[0], fib[1]);
 
 	for (i = 2; i < 98; i++)
 	{
 		fib[i] = fib[i - 1] + fib[i - 2];
 		if (i == 97)
 		{
-			printf("%ld\n", fib[i]);
+			printf("\n", fib[i]);
 		}
 		else
 		{
-			printf("%ld, ", fib[i]);
+			printf(", ", fib[i]);
 		}
 	}
 	return (0);
