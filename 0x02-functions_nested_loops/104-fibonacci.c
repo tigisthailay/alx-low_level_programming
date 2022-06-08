552bb77d32ff0a209802d44d552577b2b7c567fa
Tegisty Hailay <tigisthay13@gmail.com>
2022-06-08 06:30:37 -0700

    104-fibonacci.cy

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-	int t1 = 0, t2 = 1, t3 = 0, n = 98;
+	int t1 = 0, t2 = 1, t3 = 1, n = 98;
 
 	t3 = t1 + t2;
 
 	while (t3 <= n)
 	{
-		printf("%d ", t3);
+		printf(t3);
 		printf(',');
 		prinf(' ');
 		t1 = t2;
