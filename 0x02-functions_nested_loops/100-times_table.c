e045ee8d584ddf5f9e0487dc9f8be61ffe31eb38
Tegisty Hailay <tigisthay13@gmail.com>
2022-06-08 06:21:34 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,50 +8,39 @@
 
 void print_times_table(int n)
 {
-	int i, j;
-	
-	if (n > 0 && n < 15)
+	int i, j, res;
+
+	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
 			_putchar('0');
+
 			for (j = 1; j <= n; j++)
 			{
-				putformat(i * j);
+				_putchar(',');
+				_putchar(' ');
+				res = (i * j);
+				if (res <= 99)
+				{
+					_putchar(' ');
+				}
+				if (res <= 9)
+				{
+					_putchar(' ');
+				}
+				if (res >= 100)
+				{
+					_putchar((res / 100) + '0');
+					_putchar(((res / 10)) % 10 + '0');
+				}
+				else if (res <= 99 && res >= 10)
+				{
+					_putchar((res / 10) + '0');
+				}
+				_putchar((res % 10) + '0');
 			}
 			_putchar('\n');
 		}
-	}
-}
-/**
- * putformat - formatted characters to output
- * @n: number to format
- * Return: nothing
- */
-void putformat(int n)
-{
-	if (n <= 9)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(n + '0');
-	}
-	else if (n > 9 && n <= 99)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(n / 100 + '0');
-		_putchar(n / 10 % 10 + '0');
-		_putchar(n % 10 + '0');
 	}
 }
