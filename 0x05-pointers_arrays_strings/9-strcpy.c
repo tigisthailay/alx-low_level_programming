218f5e219fc51ae14d25e77744801f272931c948
Tegisty Hailay <tigisthay13@gmail.com>
2022-06-13 08:34:30 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,9 +9,12 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	char *aux = dest;
+	int i = -1;
 
-	while (*src)
-		*dest++ = *src++;
-	return (aux);
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0');
+
+	return (dest);
 }
