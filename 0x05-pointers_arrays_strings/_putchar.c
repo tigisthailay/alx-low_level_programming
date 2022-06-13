34f81960e193c673cb561fcd2a5b7b146b96d098
Tegisty Hailay <tigisthay13@gmail.com>
2022-06-13 04:02:47 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,35 +1,12 @@
-#include "main.h"
+#include <unistd.h>
 
 /**
- * main -  Entry point.
- * Return: Always 0.
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *.Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int main(void)
+int _putchar(char c)
 {
-	char *tg = "_putchar";
-
-	while (*tg)
-	{
-		_putchar(*tg);
-		tg++
-	}
-	_putchar('\n');
-	return (0);
-}
-
-
-			while (*tg)
-
-					{
-
-								_putchar(*tg);
-
-										tg++;
-
-											}
-
-				_putchar('\n');
-
-					return (0);
-
+	  return (write(1, &c, 1));
 }
