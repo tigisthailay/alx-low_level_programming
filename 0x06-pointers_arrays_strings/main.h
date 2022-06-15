d2a200fb0706098406c1998c4a9adaf1c20f0431
Tegisty Hailay <tigisthay13@gmail.com>
2022-06-15 10:06:10 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,3 +7,7 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *s);
 char *rot13(char *s);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+int _putchar(char c);
