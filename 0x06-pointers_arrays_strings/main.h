0ec8a4ae8969497b3258b67da2a57274b6e55dfa
Tegisty Hailay <tigisthay13@gmail.com>
2022-06-16 09:33:04 -0700

    main header files

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,3 +12,4 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 int _putchar(char c);
 int base10(int n);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
